e67622e4742a98edd3dc526c0d23dd7cc424f1ed
Kevin Schoon <kevinschoon@gmail.com>
2017-01-05 17:49:37 -0500

    Add Filter type for pagination

--- http.go
+++ http.go
@@ -50,66 +50,14 @@ func (h DefaultHandler) Handle(u *url.URL, o interface{}) error {
 	return json.Unmarshal(raw, o)
 }
 
+// Filter filters the results of a
+// paginator based on some criteria.
+type Filter func(interface{}) bool
+
 // Paginator is handles some stateful request
 type Paginator interface {
-	Next(Handler) error // Make the next HTTP request
-	Close()             // Close any open channels
-}
-
-// TaskPaginator paginates requests from /master/tasks
-// TODO: It appears that we should be able to
-// unmarshal responses from the non-scheduler API
-// with protobuf code generated from /include/mesos/master/master.proto
-// however I was unsuccesful after several attempts. Additionally
-// we would ideally want to match the vendored mesos-go protobufs.
-type TaskPaginator struct {
-	tasks chan *mesos.Task
-	count int
-	limit int
-	max   int
-	order string
-}
-
-func (t *TaskPaginator) Close() { close(t.tasks) }
-
-func (t *TaskPaginator) Next(h Handler) error {
-	u := &url.URL{
-		Path: "/master/tasks",
-		RawQuery: url.Values{
-			"offset": []string{string(t.count)},
-			"limit":  []string{string(t.limit)},
-		}.Encode(),
-	}
-	tasks := struct {
-		Tasks []struct {
-			ID          *string          `json:"id"`
-			FrameworkID *string          `json:"framework_id"`
-			AgentID     *string          `json:"slave_id"`
-			State       *mesos.TaskState `json:"state"`
-		} `json:"tasks"`
-	}{}
-	if err := h.Handle(u, &tasks); err != nil {
-		return err
-	}
-	for _, task := range tasks.Tasks {
-		t.count++
-		// Check if we've exceeded the maximum tasks
-		if t.count >= t.max {
-			return ErrMaxExceeded
-		}
-		t.tasks <- &mesos.Task{
-			TaskId:      &mesos.TaskID{Value: task.ID},
-			FrameworkId: &mesos.FrameworkID{Value: task.FrameworkID},
-			SlaveId:     &mesos.SlaveID{Value: task.AgentID},
-			State:       task.State,
-		}
-	}
-	// If the response is smaller than the limit
-	// we have finished this request
-	if len(tasks.Tasks) < t.limit {
-		return ErrEndPagination
-	}
-	return nil
+	Next(Handler, ...Filter) error // Make the next HTTP request
+	Close()                        // Close any open channels
 }
 
 // Client implements a simple HTTP client for
@@ -138,10 +86,10 @@ func (c *Client) Agents() ([]*mesos.SlaveInfo, error) {
 	return agents, nil
 }
 
-func (c *Client) Paginate(pag Paginator) (err error) {
+func (c *Client) Paginate(pag Paginator, f ...Filter) (err error) {
 	defer pag.Close()
 	for err == nil {
-		err = pag.Next(c.handler)
+		err = pag.Next(c.handler, f...)
 	}
 	switch err {
 	case ErrMaxExceeded:
--- ps.go
+++ ps.go
@@ -4,21 +4,109 @@ import (
 	"fmt"
 	"github.com/jawher/mow.cli"
 	mesos "github.com/mesos/mesos-go/mesosproto"
+	"net/url"
+	"regexp"
 )
+
+// TaskPaginator paginates requests from /master/tasks
+// TODO: It appears that we should be able to
+// unmarshal responses from the non-scheduler API
+// with protobuf code generated from /include/mesos/master/master.proto
+// however I was unsuccesful after several attempts. Additionally
+// we would ideally want to match the vendored mesos-go protobufs.
+type TaskPaginator struct {
+	tasks     chan *mesos.Task
+	processed int    // Total number of tasks proessed
+	count     int    // Total number of matching tasks
+	limit     int    // Limit of tasks per request
+	max       int    // Maximum amount of matching tasks
+	order     string // Order of tasks
+}
+
+func (t *TaskPaginator) Close() { close(t.tasks) }
+
+func (t *TaskPaginator) Next(h Handler, f ...Filter) error {
+	u := &url.URL{
+		Path: "/master/tasks",
+		RawQuery: url.Values{
+			"offset": []string{fmt.Sprintf("%d", t.processed)},
+			"limit":  []string{fmt.Sprintf("%d", t.limit)},
+		}.Encode(),
+	}
+	tasks := struct {
+		Tasks []struct {
+			ID          *string          `json:"id"`
+			Name        *string          `json:"name"`
+			FrameworkID *string          `json:"framework_id"`
+			AgentID     *string          `json:"slave_id"`
+			State       *mesos.TaskState `json:"state"`
+		} `json:"tasks"`
+	}{}
+	if err := h.Handle(u, &tasks); err != nil {
+		return err
+	}
+loop:
+	for _, other := range tasks.Tasks {
+		t.processed++
+		task := &mesos.Task{
+			TaskId:      &mesos.TaskID{Value: other.ID},
+			Name:        other.Name,
+			FrameworkId: &mesos.FrameworkID{Value: other.FrameworkID},
+			SlaveId:     &mesos.SlaveID{Value: other.AgentID},
+			State:       other.State,
+		}
+		for _, filter := range f {
+			// If any filter does not match discard the task
+			if !filter(task) {
+				continue loop
+			}
+		}
+		t.count++
+		// Check if we've exceeded the maximum tasks
+		if t.count >= t.max {
+			return ErrMaxExceeded
+		}
+		t.tasks <- task
+	}
+	// If the response is smaller than the limit
+	// we have finished this request
+	if len(tasks.Tasks) < t.limit {
+		return ErrEndPagination
+	}
+	return nil
+}
 
 func ps(cmd *cli.Cmd) {
 	cmd.Spec = "[OPTIONS]"
 	defaults := DefaultProfile()
 	var (
 		master = cmd.StringOpt("master", defaults.Master, "Mesos Master")
-		limit  = cmd.IntArg("limit", 100, "maximum number of tasks to return per request")
-		max    = cmd.IntArg("max", 250, "maximum number of tasks to list")
+		limit  = cmd.IntOpt("limit", 100, "maximum number of tasks to return per request")
+		max    = cmd.IntOpt("max", 250, "maximum number of tasks to list")
 		order  = cmd.StringArg("order", "desc", "accending or decending sort order [asc|desc]")
+		name   = cmd.StringOpt("name", "", "regular expression to match the TaskId")
+		state  = cmd.StringOpt("state", "", "regular expression to match the State")
 	)
 	cmd.Action = func() {
-		profile := config.Profile(WithMaster(*master))
+		filters := []Filter{}
+		if *name != "" {
+			exp, err := regexp.Compile(*name)
+			failOnErr(err)
+			filters = append(filters, func(t interface{}) bool {
+				task := t.(*mesos.Task)
+				return exp.MatchString(*task.Name)
+			})
+		}
+		if *state != "" {
+			exp, err := regexp.Compile(*state)
+			failOnErr(err)
+			filters = append(filters, func(t interface{}) bool {
+				task := t.(*mesos.Task)
+				return exp.MatchString(task.State.String())
+			})
+		}
 		tasks := make(chan *mesos.Task)
-		client := &Client{handler: DefaultHandler{hostname: profile.Master}}
+		client := &Client{handler: DefaultHandler{hostname: config.Profile(WithMaster(*master)).Master}}
 		paginator := &TaskPaginator{
 			limit: *limit,
 			max:   *max,
@@ -26,7 +114,7 @@ func ps(cmd *cli.Cmd) {
 			tasks: tasks,
 		}
 		go func() {
-			failOnErr(client.Paginate(paginator))
+			failOnErr(client.Paginate(paginator, filters...))
 		}()
 		// TODO: make pretty
 		for task := range tasks {
